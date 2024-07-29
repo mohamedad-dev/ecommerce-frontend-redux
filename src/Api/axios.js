1ed422508e50b58e283dabecdaa41f68b87a8bf8
mohamedad-dev <mohamedamine.dammak98@gmail.com>
2024-07-29 19:13:45 +0100

    first commit

---
+++ src/Api/axios.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+export default axios.create({
+  baseURL: 'https://ecommerce-backend-1nzl.vercel.app/api/',
+  // baseURL:" https://ecombackend-rouge.vercel.app/api/"
+})
